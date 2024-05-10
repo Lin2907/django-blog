b89ea7a20633dc97ec2f5e3e117bc5ad8bc141a4
Holinda Beshiri <holinda_b@hotmail.com>
2024-05-10 15:55:16 +0000

    Add the model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ '8000-lin2907-djangoblog-x7h6gnz04v3.ws-eu111.gitpod.io',
                   '.herokuapp.com']
