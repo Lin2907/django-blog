ceb4ecee164080fdc3fadd1b937e8f766a0d554a
Holinda Beshiri <holinda_b@hotmail.com>
2024-05-13 19:57:09 +0000

    Add allauth and accoun verification

--- blog/models.py
+++ blog/models.py
@@ -1,44 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-    content = models.TextField(default="")
+        User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    
+    excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ['-created_on']
 
     def __str__(self):
-        return f"{self.title} | written by {self.author}"
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="comments_author"
-    )
+        Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ['created_on']
 
     def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+        return f"Comment {self.body} by {self.name}"
--- blog/views.py
+++ blog/views.py
@@ -1,55 +1,81 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
-from .models import Event
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .models import Post
+#from .forms import CommentForm
 
-# Create your views here.
+
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
 
-def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
 
-    **Context**
+class PostDetail(View):
 
-    ``post``
-        An instance of :model:`blog.Post`.
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-    **Template:**
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
-    :template:`blog/post_detail.html`
-    """
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+        comment_form = CommentForm(data=request.POST)
 
-    return render(
-    request,
-    "blog/post_detail.html",
-    {"post": post},
-)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            messages.add_message(request, messages.SUCCESS, 'Your comment was\
+                posted successfully and is awaiting for approval')
+            comment.save()
+        else:
+            comment_form = CommentForm()
 
-    class EventsList(generic.ListView):
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
 
-      model = Event
-      template_name = "index.html"
-      paginate_by = 12
+class PostLike(View):
 
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
 
-
-
-def event_detail(request, event_id):
-    
-    queryset = Event.objects.all()
-    event = get_object_or_404(queryset, event_id=event_id)
-
-    return render(
-        request,
-        "events/event_detail.html",
-        {"event": event}
-    )
-
-   
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
